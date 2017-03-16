f547cc41230c7e8dec0115ecbbb46e87afc4981a
dreedwalker <B00076316@student.itb.ie>
2017-03-16 16:38:45 +0000

    Add files via upload

---
+++ AceVenturasBarberShop/src/db/Booking.java
@@ -0,0 +1,96 @@
+/**
+ * Ace Venturas Detective Agency
+ * @author B00076316 Liam Walsh & B00091655 Seán Scott
+ *
+ */
+public class Booking {
+	private int bookingID;
+	private String email;
+	private String time;
+	private String date;
+	private int userID;
+	private boolean checkedIn;
+	
+	
+	public Booking(String time, String date) {
+		this.time = time;
+		this.date = date;
+	}
+	
+	
+	public Booking(int bookingID, String email, String time, String date, int userID) {
+		this.bookingID = bookingID;
+		this.email = email;
+		this.date = date;
+		this.time = time;
+		this.userID = userID;
+	}
+	
+	/**
+	 * @return the checkedIn
+	 */
+	public boolean isCheckedIn() {
+		return checkedIn;
+	}
+
+	/**
+	 * @param checkedIn the checkedIn to set
+	 */
+	public void setCheckedIn(boolean checkedIn) {
+		this.checkedIn = checkedIn;
+	}
+
+	
+	public int getBookingID() {
+		return bookingID;
+	}
+
+	
+	public void setBookingID(int bookingID) {
+		this.bookingID = bookingID;
+	}
+
+	
+	public String getEmail() {
+		return email;
+	}
+
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	public String getDate() {
+		return date;
+	}
+
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	
+	public String getTime() {
+		return time;
+	}
+
+	
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	/**
+	 * @return the userID
+	 */
+	public int getUserID() {
+		return userID;
+	}
+
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+}
---
+++ AceVenturasBarberShop/src/db/User.java
@@ -0,0 +1,95 @@
+/**
+ * Ace Venturas Detective Agency
+ * @author B00076316 Liam Walsh & B00091655 Seán Scott
+ *
+ */
+public class User {
+	private int userID;
+	private String email;
+	private String password;
+	private String phoneNum;
+	private boolean checkedIn;
+	
+
+	
+	/**
+	 * Constructs a booking with all information
+	 * @param bookingID Booking ID
+	 * @param roomID Room ID
+	 * @param startDate Start date
+	 * @param endDate End date
+	 * @param userID User ID
+	 * @param checkedIn Is checked in
+	 */
+	public User(int userID, String email, String phoneNum) {
+		this.userID = userID;
+		this.email = email;
+		this.phoneNum = phoneNum;
+	}
+	
+	/**
+	 * @return the checkedIn
+	 */
+	public boolean isCheckedIn() {
+		return checkedIn;
+	}
+
+	/**
+	 * @param checkedIn the checkedIn to set
+	 */
+	public void setCheckedIn(boolean checkedIn) {
+		this.checkedIn = checkedIn;
+	}
+
+	
+	public int getUserID() {
+		return userID;
+	}
+
+	
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param roomID the roomID to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the startDate
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param startDate the startDate to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the endDate
+	 */
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	/**
+	 * @param endDate the endDate to set
+	 */
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+}
+
